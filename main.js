f5200d3ae65bb2f7a357a993bd0d64b904d36d7a
SONJA <hologramm@posteo.de>
2021-01-09 18:18:48 +0100

    Intro to vue 3
     // webshop app

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            cart: [],
+            premium: true,
+        };
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        },
+        // solution
+        removeById(id) {
+            const index = this.cart.indexOf(id)
+            if (index > -1) {
+                this.cart.splice(index, 1)
+            }
         }
-    }
-})
+    },
+});
